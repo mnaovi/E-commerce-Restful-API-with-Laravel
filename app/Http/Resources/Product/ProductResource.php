e69127d28e1073cbeea6407c69e3b3fed46bf489
mnaovi <mohammadnazmulahsanovi@gmail.com>
2019-10-30 13:47:16 +0600

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
            'name'        => $this->name,
            'description' => $this->detail,
            'price'       => $this->price,
-           'stock'       => $this->stock,
-           'discount'    => $this->discount
+           'stock'       => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+           'discount'    => $this->discount,
+           'totalPrice'  => $this->price - $this->discount,
+           'rating'      => $this->reviews->count() > 0 ?round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No rating yet',
+           'href' => [
+              'reviews'  => route('reviews.index', $this->id)
+           ]
         ];
     }
 }
